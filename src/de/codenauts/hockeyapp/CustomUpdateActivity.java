3c71becb0c15adc7cdb1fe8b23ad9524e852a22d
Thomas Dohmke <thomas@dohmke.de>
2011-12-10 17:02:32 +0100

    Fixed color of action button on HoneyComb and newer. Fixed icon size on app view.

--- src/de/codenauts/hockeyapp/CustomUpdateActivity.java
+++ src/de/codenauts/hockeyapp/CustomUpdateActivity.java
@@ -1,8 +1,12 @@
 package de.codenauts.hockeyapp;
 
 import de.codenauts.hockeyapp.util.ActivityHelper;
+import de.codenauts.hockeyapp.util.UIUtils;
+import android.app.ActionBar;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
 import net.hockeyapp.android.R;
 import net.hockeyapp.android.UpdateActivity;
 
@@ -12,8 +16,27 @@ public class CustomUpdateActivity extends UpdateActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    setTitle("Version Info");
+    setTitle(null);
     activityHelper.setupActionBar(null, Color.BLACK);
+    
+    if (UIUtils.isHoneycomb()) {
+      ActionBar actionBar = getActionBar();
+      actionBar.setDisplayHomeAsUpEnabled(true);
+      actionBar.setDisplayShowTitleEnabled(false);
+    }
+  }
+  
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+      switch (item.getItemId()) {
+          case android.R.id.home:
+              Intent intent = new Intent(this, MainActivity.class);
+              intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+              startActivity(intent);
+              return true;
+          default:
+              return super.onOptionsItemSelected(item);
+      }
   }
   
   public int getLayout() {
