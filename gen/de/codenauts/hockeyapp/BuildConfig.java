1469181e1d7ffb7efb49de1fe1fc9c138d143196
Thomas Dohmke <thomas@dohmke.de>
2012-11-09 15:07:27 +0100

    Move repo.

--- gen/de/codenauts/hockeyapp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package de.codenauts.hockeyapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/main/java/de/codenauts/hockeyapp/Constants.java
+++
@@ -1,5 +0,0 @@
-package de.codenauts.hockeyapp;
-
-public class Constants {
-  public final static boolean DEBUG = false;
-}
